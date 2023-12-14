0ac1db67aca674e7b45640cbccc06bc80ef4d84d
RaunakShrest <110533553+RaunakShrest@users.noreply.github.com>
2023-12-14 10:53:35 +0545

    created registration api

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,41 @@
+const e = require("express");
+const asyncHandler= require("express-async-handler");
+const { off } = require("../models/userModel");
+const User = require("../models/userModel");
+
+const registerUser =asyncHandler(async(req,res)=>{
+const{name, email, password, pic}= req.body;
+
+
+if(!name || email|| password){
+    resizeBy.status(400);
+    throw new Error("Please enter all the fields")
+}
+const userExists= await User.findOne({email})
+
+if(userExists){
+    res.status(400);
+    throw new Error("User already exists")
+}
+
+const user= await User.create({
+    name,
+    email,
+    password,
+    pic,
+})
+if(user){
+    res.status(201).json({ 
+        //.201= success
+        _id: user._id,
+        name:user.name,
+        email:user.email,
+        pic:user.pic,
+
+    })
+} else{
+    res.status(400)
+    throw new Error("User Failed to create ")
+}
+})
+module.exports={registerUser};
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const {chats}= require("./data/data");
 const dotenv= require("dotenv");
 const connectDB = require("./config/db");
 const colors= require("colors")
+const userRoutes= require("./routes/userRoutes")
 const app= express()
 dotenv.config();
 connectDB()
@@ -12,16 +13,18 @@ app.get("/",(req,res)=>{
 res.send("API is Running");
 });
 
-app.get('/api/chat',(req,res)=>{
+// app.get('/api/chat',(req,res)=>{
 
-    res.send(chats)
-})
+//     res.send(chats)
+// })
 
-app.get('/api/chat/:id',(req,res)=>{
- //console.log(req.params.id)
- const singleChat=chats.find((c)=>c._id===req.params.id)
- res.send(singleChat)
-})
+// app.get('/api/chat/:id',(req,res)=>{
+//  //console.log(req.params.id)
+//  const singleChat=chats.find((c)=>c._id===req.params.id)
+//  res.send(singleChat)
+// })
+
+app.use('/api/user',userRoutes)
 
 const PORT= process.env.PORT || 5000
 app.listen(5000, console.log(`Sever started on PORT: ${PORT}`.blue.bold));
