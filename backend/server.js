ea962202ded371203f681ee428d9d37c12e67251
RaunakShrest <110533553+RaunakShrest@users.noreply.github.com>
2024-01-07 16:53:57 +0545

    created error handling middlewares

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const dotenv= require("dotenv");
 const connectDB = require("./config/db");
 const colors= require("colors")
 const userRoutes= require("./routes/userRoutes")
+const {notFound,errorHandler}=require("./middlewares/errorMiddleware")
 
 dotenv.config();
 connectDB()
@@ -16,18 +17,11 @@ app.get("/",(req,res)=>{
 res.send("API is Running");
 });
 
-// app.get('/api/chat',(req,res)=>{
-
-//     res.send(chats)
-// })
-
-// app.get('/api/chat/:id',(req,res)=>{
-//  //console.log(req.params.id)
-//  const singleChat=chats.find((c)=>c._id===req.params.id)
-//  res.send(singleChat)
-// })
 
 app.use('/api/user',userRoutes)
+
+app.use(notFound) // if all of the above url does not exist its gonna fall to this function
+app.use(errorHandler)
 
 const PORT= process.env.PORT || 5000
 app.listen(5000, console.log(`Sever started on PORT: ${PORT}`.blue.bold));
