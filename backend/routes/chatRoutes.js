40980f0ecf98a04e94cf88f76e3a0146c3ee8b24
RaunakShrest <110533553+RaunakShrest@users.noreply.github.com>
2024-01-09 17:17:02 +0545

    Created groupchatAPI

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,10 +1,10 @@
 const express= require('express')
 const {protect} = require("../middlewares/authMiddleware")
-const {accessChat,fetchChats}= require("../controllers/chatControllers")
+const {accessChat,fetchChats,createGroupChat}= require("../controllers/chatControllers")
 const router= express.Router();
  router.route('/').post(protect,accessChat) // protect middleware because if the user is not loged In the user cannot access
  router.route('/').get(protect,fetchChats) 
-// router.route("/group").post(protect,createGroupChat)
+router.route("/group").post(protect,createGroupChat)
 // router.route("/rename").put(protect,renameGroup)
 // router.route("/groupremove").put(protect,removeFromGroup)
 // router.route("/groupadd").put(protect,addToGroup)
