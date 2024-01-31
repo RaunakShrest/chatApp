f71b3efd981d50d5d0df88f9f9d03537a9fcbe6d
RaunakShrest <110533553+RaunakShrest@users.noreply.github.com>
2024-01-31 18:46:53 +0545

    sending messsageAPI

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,12 +1,14 @@
 const express= require('express')
 const { protect } = require('../middlewares/authMiddleware')
-
+const {sendMessage} = require("../controllers/messageControllers")
 const router= express.Router()
 
 
-// router.route('/').post(protect,sendMessage)
+router.route('/').post(protect,sendMessage)
 
 //for fetching the message for one single chat
 // router.route('/:chatId').get(protect,allMessages) 
+
+
 
 module.exports=router
